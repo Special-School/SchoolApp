14257b7093f7c957ec13ad08e69f436aaa307006
mobulabs <mobulabs@gmail.com>
2020-11-20 17:29:35 +0900

    Update libraries

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -7,15 +7,15 @@ object Versions {
     const val TARGET_SDK = 30
     const val MIN_SDK = 21
 
-    const val ANDROID_GRADLE_PLUGIN = "4.1.0"
+    const val ANDROID_GRADLE_PLUGIN = "4.1.1"
     const val APPCOMPAT = "1.2.0"
-    const val CONSTRAINT_LAYOUT = "2.0.2"
+    const val CONSTRAINT_LAYOUT = "2.0.4"
     const val CORE_KTX = "1.3.2"
     const val ESPRESSO_CORE = "3.3.0"
     const val EXT_JUNIT = "1.1.2"
     const val HAMCREST = "2.2"
     const val HILT = "2.28-alpha"
-    const val HILT_JETPACK = "1.0.0-alpha01"
+    const val HILT_JETPACK = "1.0.0-alpha02"
     const val JUNIT = "4.12"
     const val KOTLIN = "1.4.10"
     const val MATERIAL = "1.2.1"
@@ -23,5 +23,5 @@ object Versions {
     const val MOCKITO_KOTLIN = "2.2.0"
     const val NAVIGATION_FRAGMENT_KTX = "2.3.1"
     const val NAVIGATION_UI_KTX = "2.3.1"
-    const val ROOM_VERSION = "2.2.5"
+    const val ROOM_VERSION = "2.3.0-alpha03"
 }
