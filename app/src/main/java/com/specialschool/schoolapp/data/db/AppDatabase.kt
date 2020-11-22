e20d864ddaa99e93438ea1f81b5d30ebd60891ac
mobulabs <mobulabs@gmail.com>
2020-11-22 13:15:10 +0900

    Full text search를 위한 Room 설정

--- app/src/main/java/com/specialschool/schoolapp/data/db/AppDatabase.kt
+++ app/src/main/java/com/specialschool/schoolapp/data/db/AppDatabase.kt
@@ -1,15 +1,14 @@
-package com.specialschool.schoolapp.data
+package com.specialschool.schoolapp.data.db
 
 import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-// For room test
-@Database(entities = [UserEntity::class], version = 1, exportSchema = false)
+@Database(entities = [SchoolFtsEntity::class], version = 2, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
 
-    abstract fun userDao(): UserDao
+    abstract fun schoolDao(): SchoolDao
 
     companion object {
         fun buildDatabase(context: Context): AppDatabase {
