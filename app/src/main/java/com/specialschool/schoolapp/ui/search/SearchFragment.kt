c6820f4c06f8ab932940ac4664b836e365b789f1
mobulabs <mobulabs@gmail.com>
2020-12-11 19:43:32 +0900

    19:43 commit

--- app/src/main/java/com/specialschool/schoolapp/ui/search/SearchFragment.kt
+++ app/src/main/java/com/specialschool/schoolapp/ui/search/SearchFragment.kt
@@ -29,12 +29,6 @@ class SearchFragment : Fragment() {
 
     private lateinit var binding: FragmentSearchBinding
 
-    private val viewModel: SearchViewModel by viewModels()
-
-    lateinit var recyclerSchool: RecyclerView
-    private val testArray: ArrayList<Memo> = ArrayList()
-    private val searchArray: ArrayList<Memo> = ArrayList()
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -72,37 +66,5 @@ class SearchFragment : Fragment() {
     private fun dismissKeyboard(view: View) {
         val imm = view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken, 0)
-    }
-
-
-    class SearchViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-
-        fun bindItem(data: Memo) {
-            itemView.school_main.text = data.schoolName
-            itemView.school_name.text = data.addrDetail
-
-            itemView.setOnClickListener {
-                //item 터치시 토스트 메시지 생성
-                Toast.makeText(
-                    itemView.context,
-                    "${data.schoolName}의 세부 정보 화면으로 이동합니다.",
-                    Toast.LENGTH_SHORT
-                ).show()
-
-                val intent = Intent(itemView?.context, SearchTestActivity::class.java)
-                var intent_city: Intent = intent.putExtra("city", data.city)
-                var intent_establish: Intent = intent.putExtra("establish", data.establish)
-                var intent_school_name: Intent = intent.putExtra("school_name", data.schoolName)
-                var intent_type = intent.putExtra("type", data.type)
-                var intent_opening: Intent = intent.putExtra("opening", data.openDate)
-                var intent_tel_num: Intent = intent.putExtra("tel1", data.tel1)
-                var intent_tel: Intent = intent.putExtra("tel2", data.tel2)
-                var intent_addr_num: Intent = intent.putExtra("addr_num", data.addrNum)
-                var intent_addr_detail: Intent = intent.putExtra("addr_detail", data.addrDetail)
-                var intent_url: Intent = intent.putExtra("url", data.url)
-
-                startActivity(itemView.context, intent, null)
-            }
-        }
     }
 }
--- app/src/main/java/com/specialschool/schoolapp/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/specialschool/schoolapp/ui/search/SearchViewModel.kt
@@ -1,11 +1,46 @@
 package com.specialschool.schoolapp.ui.search
 
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import com.specialschool.schoolapp.domain.search.SearchUseCase
+import com.specialschool.schoolapp.model.School
+import com.specialschool.schoolapp.util.Event
+import com.specialschool.schoolapp.util.Result
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
-class SearchViewModel @ViewModelInject constructor() : ViewModel() {
+class SearchViewModel @ViewModelInject constructor(
+    private val searchUseCase: SearchUseCase
+) : ViewModel() {
 
+    private val _navigateToSchoolDetailAction = MutableLiveData<Event<String>>()
+    val navigateToSchoolDetailAction: LiveData<Event<String>> = _navigateToSchoolDetailAction
 
+    private val _searchResults = MediatorLiveData<List<School>>()
+    val searchResult: LiveData<List<School>> = _searchResults
+
+    private var searchJob: Job? = null
+
+    fun onSearchQueryChanged(query: String) {
+        if (query.length < 2) {
+            onQueryCleared()
+            return
+        }
+        executeSearch(query)
+    }
+
+    private fun executeSearch(query: String) {
+        viewModelScope.launch {
+            processSearchResult(searchUseCase(query.trim()))
+        }
+    }
+
+    private fun processSearchResult(result: Result<List<School>>) {
+        val searchResults = (result as? Result.Success)?.data ?: emptyList()
+        _searchResults.value = searchResults
+    }
+
+    private fun onQueryCleared() {
+        _searchResults.value = emptyList()
+    }
 }
