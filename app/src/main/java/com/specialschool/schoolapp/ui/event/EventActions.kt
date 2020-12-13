5aa61f82ca1515649d2036404da5c5307c046272
mobulabs <mobulabs@gmail.com>
2020-12-13 12:27:55 +0900

    Fix error

--- app/src/main/java/com/specialschool/schoolapp/ui/event/EventActions.kt
+++ app/src/main/java/com/specialschool/schoolapp/ui/event/EventActions.kt
@@ -1,9 +1,5 @@
 package com.specialschool.schoolapp.ui.event
 
-import com.specialschool.schoolapp.ui.search.SearchTestActivity
-import com.specialschool.schoolapp.ui.search.SearchViewModel
-
 interface EventActions {
     fun openItemDetail(id: String)
-
 }
